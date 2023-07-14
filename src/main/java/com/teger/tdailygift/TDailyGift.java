d0c4c8798932e8d73c1c64ef95090f719931c44a
jy2694 <wpdus2694@naver.com>
2023-07-14 20:36:14 +0900

    Initial Commit

---
+++ src/main/java/com/teger/tdailygift/TDailyGift.java
@@ -0,0 +1,17 @@
+package com.teger.tdailygift;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TDailyGift extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
